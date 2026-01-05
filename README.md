7d1d766ac3deef1103d1bd5f7c847e0e5dc5ef84
marissadolorfino2024 <marissadolorfino2024@gmail.com>
2026-01-05 13:26:18 -0500

    add scripts for building BB dictionaries and splitting data for training

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Package for processing DNA-encoded library data, training ML models, and picking hits from make on demand libraries
 
+Order to run scripts:
+  - ECFP4calculator.py
+  - Make_BBdictionaries.py
+  - Split_TestTrainVal.py
+  - Run_Allmodels.py
+
 Machine learning model descriptions and usage for models in `models.py`:
   - ...
   - ...
--- Run_Allmodels.py
+++ Run_Allmodels.py
@@ -11,6 +11,7 @@ import argparse
 import sys 
 
 from utils import *
+from models import *
 
 class HelpAction(argparse.Action):
     def __init__(self, option_strings, dest, **kwargs):
@@ -27,32 +28,33 @@ Arguments:
     output_dir  - Output directory path
 
 Options:
-    -e, --epochs INT         - Specify the number of epochs for training (default is to scan 10, 100, 500)
-    -b, --batchsize INT      - Specify the batchsize for training (default is to scan 500, 1000, 10000)
-    -l, --learningrate FLOAT - Specify the (default is to scan 0.001, 0.01, 0.1)
-    -m, --models STR         - Specify the names of models you'd like to train (default is to train them all, except for the read count model, which can be trained by specifying the -r option)
-    -f, --fullmolecule       - Only run the full molecule models (default: False)
-    -r, --readcount          - Run the read count model (default: False)
-    -h, --help               - Show this help message
+    -e, --epochs INT                              - Specify the number of epochs for training (default is to scan 10, 100, 500)
+    -b, --batchsize INT                           - Specify the batchsize for training (default is to scan 500, 1000, 10000)
+    -l, --learningrate FLOAT                      - Specify the learning rate for training (default is to scan 0.001, 0.01, 0.1)
+    -m, --models (space delimited list of models) - Specify the names of models you'd like to train (default is to train them all, except for the read count model, which can be trained by specifying the -r option)
+    -f, --fullmolecule                            - Only run the full molecule models (default: False)
+    -r, --readcount                               - Run the read count model (default: False)
+    -h, --help                                    - Show this help message
 """
         print(help_text)
         sys.exit(0)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', help="file path to the train csv file, which contains a column called 'molecule_smiles' and 'building_block{i}_smiles'... if -b is specified")
+    parser.add_argument('filename', help="the parquet file output from running ecfp4_calculator.py")
     parser.add_argument('output_dir', help="path to the output directory")
-    parser.add_argument('-c', '--chunk_size', type=int, default=500000, help='change the chunk_size for reading in the input csv (default is 500,000)')
-    parser.add_argument('-s', '--ecfp4_size', type=int, default=1024, help='change the size of ECFP4 fingerprints (default is 1024)')
-    parser.add_argument('-r', '--remove_dy', action='store_true', help='if specified, the Dy tag found in some DELs will be removed and replaced with a PEG linker')
-    parser.add_argument('-b', 'bb_fingerprints', action='store_true', help='if specified, fingerprints will be calculated for building blocks as well.')
+    parser.add_argument('e', '--epochs', type=int, help='specify the number of epochs for training models. the default is to scan epochs=10,100,500')
+    parser.add_argument('b', '--batchsize', type=int, help='specify the batchsize for training models. the default is to scan batchsize=500,1000,10000')
+    parser.add_argument('l', '--learningrate', type=float, help='specify the learning rate for training. the default is to scan lr=0.001,0.01,0.1')
+    parser.add_argument('m', '--models', nargs='+', help='list of models (identified by name in models.py) that you want to run. the default is to run all of them except for the read count model')
+    parser.add_argument('f', '--fullmolecule', action='store_true', help='if specified, only the full molecule model will be run')
+    parser.add_argument('r', '--readcount', action='store_true', help="if specified, the readcount model will be run. there must be a 'readcount' column in the file if this option is specified. the default is false.")
     parser.add_argument('-h', '--help', action=HelpAction)
     
     args = parser.parse_args()
 
 if __name__ == '__main__':
     main()
-
 
 
 
