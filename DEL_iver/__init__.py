e81dde74de022a891f6081a2df94d5cd4c735be9
Desp-Chem <dsanper@umich.edu>
2026-04-02 20:56:33 -0400

    rewrote data reader to take parquet only if csv is given it loads a cache instead, also now generates a clearly formatted warnings thanks to function in central __init__

--- DEL_iver/__init__.py
+++ DEL_iver/__init__.py
@@ -1,9 +1,16 @@
 # DEL_iver/__init__.py
 
 #Any function listed here will be callable with no imports needed other then 
-
 # Import everything you want available at the top level when you run import DEL_iver
 from .data_loader.data_reader import Data_Reader
 from .data_loader.Split_TestTrainVal import split_data
+#from .molecules.Make_BBdictionaries import generate_BB_dictionaries
 
 
+
+import warnings
+
+def _custom_warning_format(message, category, filename, lineno, file=None, line=None):
+    return f"\n[DEL-iver] {category.__name__}: {message}\n"
+
+warnings.formatwarning = _custom_warning_format
